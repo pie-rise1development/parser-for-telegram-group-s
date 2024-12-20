f09f10b2a176ec528de576202ec7b05eff62f4b3
p-RiseDev <dizelano.53.usinger.917@yandex.ru>
2024-12-20 21:04:25 +0300

    Add files via upload

--- parser-of-telegram-groups-1.py
+++ parser-of-telegram-groups-1.py
@@ -1,18 +1,19 @@
 from telethon import TelegramClient
 import asyncio
 
-# Вставьте свои данные
-api_id = ''
-api_hash = ''
-group_username = ''  # Имя группы или её ID
-output_file = 'selected.txt'
+# Вставьте свои данные.
+# Чтобы получить данные для ввода в переменные ниже, воспользуйтесь сайтом "my.telegram.org" (Авторизация ---> Создание приложения ---> Получение от него данных.)
+api_id = ""
+api_hash = ""
+username_of_your_group = "" # Имя группы или её ID.
+output_file = "selected.txt" # Название файла, куда будет сохраняться парсинг имён из Telegram группы.
 
 async def main():
     async with TelegramClient('session_name', api_id, api_hash) as client:
-        # Получаем участников группы
-        participants = await client.get_participants(group_username)
-        
-        # Сохраняем никнеймы в файл
+        # Получаем участников группы.
+        participants = await client.get_parcitipant(username_of_your_group)
+
+        # Сохраняем полученные ник-неймы в файл ".txt".
         with open(output_file, 'w', encoding='utf-8') as f:
             for user in participants:
                 if user.username:
@@ -20,7 +21,7 @@ async def main():
                 else:
                     f.write(f"{user.first_name} {user.last_name}\n")
 
-# Запускаем асинхронную функцию
+# Проверка работы написанного кода.
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
 
